96aa2f92e3694af5b3027aae970d3fa515c73c89
Milad Heidari <milad.h9999@gmail.com>
2024-12-02 17:17:35 -0800

    Change: models

--- controller/models.py
+++ controller/models.py
@@ -88,50 +88,31 @@ class TransitionModel(nn.Module):
         return state
     
 
-class PosteriorModel(nn.Module):
+class EncoderModel(nn.Module):
     """
-        s_t = g(o1:t, a1:t-1)
+        s_t = g(ot)
     """
 
     def __init__(
         self,
         observation_dim: int,
-        action_dim: int,
         state_dim: int,
-        rnn_hidden_dim: int,
-        rnn_input_dim: int,
+        hidden_dim: int,
     ):
         super().__init__()
 
-        # RNN hidden at time t summarizes o1:t-1, a1:t-2
-        self.rnn = nn.GRUCell(
-            input_size=rnn_input_dim,
-            hidden_size=rnn_hidden_dim,
-        )
+        hidden_dim = hidden_dim if hidden_dim is not None else 2*observation_dim
 
-        self.fc_obs_action = nn.Sequential(
-            nn.Linear(observation_dim + action_dim, rnn_input_dim),
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(observation_dim, hidden_dim),
             nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, state_dim),
         )
-
-        self.state_head = nn.Linear(rnn_hidden_dim, state_dim)
-
-        self.rnn_hidden_dim = rnn_hidden_dim
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.rnn_input_dim = rnn_input_dim
 
     def forward(
         self,
-        prev_rnn_hidden,
-        prev_action,
         observation,
     ):
-        rnn_input = self.fc_obs_action(
-            torch.cat([observation, prev_action], dim=1)
-        )
-        rnn_hidden = self.rnn(rnn_input, prev_rnn_hidden)
-
-        state = self.state_head(rnn_hidden)
-
-        return rnn_hidden, state
+        return self.mlp_layers(observation)
